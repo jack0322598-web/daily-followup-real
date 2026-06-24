c0eced8ff9ddaa9e018cf0caa984e634ca470d50
jack0322598 <jack0322598@gmail.com>
2026-06-24 09:30:29 +0900

    Remove ImpactAlpha impact source

--- test_impact_sources.py
+++ test_impact_sources.py
@@ -5,14 +5,14 @@ from unittest.mock import patch
 import main
 
 
-class ImpactAlphaFeedTests(unittest.TestCase):
+class GlobalImpactFeedTests(unittest.TestCase):
     def test_fetch_global_impact_preserves_rss_link_text(self):
         feed = """<?xml version="1.0" encoding="UTF-8"?>
         <rss version="2.0">
           <channel>
             <item>
               <title>Impact investing expands into a new market</title>
-              <link>https://impactalpha.com/example-impact-story/</link>
+              <link>https://example.com/example-impact-story/</link>
               <pubDate>Fri, 19 Jun 2026 11:12:00 +0000</pubDate>
               <description><![CDATA[An impact investing article with useful details.]]></description>
             </item>
@@ -21,7 +21,7 @@ class ImpactAlphaFeedTests(unittest.TestCase):
         </rss>"""
 
         with (
-            patch.object(main, "GLOBAL_IMPACT_FEEDS", [("ImpactAlpha", "https://impactalpha.com/feed/")]),
+            patch.object(main, "GLOBAL_IMPACT_FEEDS", [("Example Impact", "https://example.com/feed/")]),
             patch.object(main, "fetch_text", return_value=feed),
             patch.object(main, "fetch_article_body_text", return_value=""),
             patch.object(main, "make_three_line_summary", return_value=["one", "two", "three"]),
@@ -29,8 +29,8 @@ class ImpactAlphaFeedTests(unittest.TestCase):
             items = main.fetch_global_impact(date(2026, 6, 19), set(), [])
 
         self.assertEqual(len(items), 1)
-        self.assertEqual(items[0]["source"], "ImpactAlpha")
-        self.assertEqual(items[0]["link"], "https://impactalpha.com/example-impact-story/")
+        self.assertEqual(items[0]["source"], "Example Impact")
+        self.assertEqual(items[0]["link"], "https://example.com/example-impact-story/")
         self.assertEqual(items[0]["date"], "2026.06.19")
 
 
